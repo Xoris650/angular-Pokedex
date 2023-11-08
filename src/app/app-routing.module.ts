9642510b5df51199be8bdfd3b801b4861756059d
Xoris650 <gabrielousadia12@gmail.com>
2023-11-08 11:29:17 -0300

    creating basic informations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PokePageComponent } from './pages/poke-page/poke-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: ':id', component: PokePageComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokeCardComponent } from './components/poke-card/poke-card.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PokePageComponent } from './pages/poke-page/poke-page.component';
+import { PokeInfoComponent } from './components/poke-info/poke-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokeCardComponent,
-    HomeComponent
+    HomeComponent,
+    PokePageComponent,
+    PokeInfoComponent
   ],
   imports: [
     BrowserModule,
